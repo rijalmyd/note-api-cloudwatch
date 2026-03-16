644ecf82b352d6f9382922073a1a05b0f2c7aee0
Rijal Muhyidin <muhyidinrijal@gmail.com>
2026-03-16 17:13:38 +0700

    update: host and port to .env

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import notes from './api/notes/routes.js';
-import dotenv from 'dotenv';
+import dotenv, { config } from 'dotenv';
 
 dotenv.config();
 
 import loggingMiddleware from './logger/middleware.js';
 
 const app = express();
-const port = 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.HOST;
+const port = process.env.PORT || 3000;
 
 app.use(loggingMiddleware);
 app.use(express.json());
 app.use('/', notes);
 
-app.listen(port, () => {
-  console.log(`server start at http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`server start at http://${host}:${port}`);
 });
